ebe4db151bf64c5d3c1a425ace9f8a30d028e626
diyigemt <qwe1355247243@gmail.com>
2021-05-23 17:43:49 +0800

    更多的注释

--- src/main/java/org/miraiboot/interfaces/EventHandlerNext.java
+++ src/main/java/org/miraiboot/interfaces/EventHandlerNext.java
@@ -1,7 +1,6 @@
 package org.miraiboot.interfaces;
 
 import net.mamoe.mirai.event.ListeningStatus;
-import net.mamoe.mirai.event.events.MessageEvent;
 import org.miraiboot.entity.MessageEventPack;
 import org.miraiboot.entity.PreProcessorData;
 
