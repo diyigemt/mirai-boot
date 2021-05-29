ecb784188eb889e60e4c4393341430d4e958fde0
diyigemt <qwe1355247243@gmail.com>
2021-05-29 11:21:18 +0800

    开始实现插件模式

--- src/main/java/net/diyigemt/miraiboot/Main.java
+++ src/main/java/net/diyigemt/miraiboot/Main.java
@@ -2,10 +2,14 @@ package net.diyigemt.miraiboot;
 
 import net.diyigemt.miraiboot.annotation.MiraiBootApplication;
 import net.diyigemt.miraiboot.autoconfig.MiraiApplication;
+import net.diyigemt.miraiboot.autoconfig.PluginLoader;
+
+import java.util.List;
 
 @MiraiBootApplication(description = "测试项目")
 public class Main {
   public static void main(String[] args) {
+    List<Class<?>> classes = PluginLoader.getPluginClasses();
     MiraiApplication.run(Main.class, args);
   }
 }
